d84385b5ade53cdda59bc960f0c67d8e84d02461
liyiyang1854 <liyiyanglinch@gmail.com>
2016-03-17 01:36:37 -0400

    solve the difference position between two servos
    for example, pos_ser2 = 1024 - pos_ser3

--- dynaPoseSerial/demo/demo.ino
+++ dynaPoseSerial/demo/demo.ino
@@ -9,7 +9,7 @@ boolean single_servo;
 int servo_num2;
 boolean run = true;
 int digits = 0;
-int innumber;
+long innumber;
 //===========================================================================
 void setup()
 {
@@ -18,7 +18,7 @@ void setup()
   delay(100);
   
   SetPosition(1,512);
-  Serial.println("setting up done");
+  Serial.println("Set up done");
   delay(1000);
 }
 
@@ -27,7 +27,8 @@ void setup()
 //============================================================================
 void loop() 
 {
-  
+  delay(1000);
+  Serial.println("loop");
   if (Serial.available() > 0) 
   {
     Serial.println("get Serial");
@@ -35,12 +36,14 @@ void loop()
     instring = Serial.readString();
     
     // say what you got:
-    
+    Serial.print("I received: ");
+    Serial.print(instring);
     innumber = instring.toInt(); 
     Serial.print("instring in integer: ");
     Serial.println(innumber);
+    Serial.println("-----------------------------------");
     
-    int temp = innumber;
+    long temp = innumber;
     digits = 0;
     while (temp)
     {
@@ -51,7 +54,7 @@ void loop()
     Serial.print("temp size: ");
     Serial.println(digits);
     Serial.print("instring length: ");
-    Serial.println(instring.length());
+    Serial.println(instring.length()-1); //instring has one more digit
     
     if (digits != (instring.length()-1))
     {
@@ -60,6 +63,10 @@ void loop()
       Serial.println("instring has char, input number");
     }
     
+    Serial.print("char check, run: ");
+    Serial.println(run);
+    Serial.println("-----------------------------------");
+    
     if (digits < 2 && run)
     {
       //length check
@@ -67,19 +74,22 @@ void loop()
       Serial.println("input more digits");
     }
     else if (digits > 1 && run)
-    {
-      Serial.print("I received: ");
-      Serial.print(instring);   
+    {   
       char c = instring[0];
       servo_num = (int)c-48;
-      Serial.print("servo number: ");
+      Serial.print("first servo number: ");
       Serial.println(servo_num);
     }
+    
+    Serial.print("instring length check, run: ");
+    Serial.println(run);
+    Serial.println("-----------------------------------");
     
     if ((servo_num == 3 || servo_num == 5) && run)
     {
       //error check
       run = false;
+      Serial.println("first servo is 3 or 5");
     }
     
     if (run)
@@ -88,6 +98,9 @@ void loop()
       Serial.print("current position for first servo: ");
       Serial.println(cur_pos);
     }
+    Serial.print("current position check, run: ");
+    Serial.println(run);
+    Serial.println("-----------------------------------");
     
     if ((servo_num == 1 || servo_num == 6 || servo_num == 8) && run)
     {
@@ -101,16 +114,19 @@ void loop()
       desire_pos = innumber - s;
       Serial.print("desired postion: ");
       Serial.println(desire_pos);
+      Serial.println("-----------------------------------");
     
     }
     else if ((servo_num == 2 || servo_num == 4) && run)
     {
+      Serial.println("first servo is 2 or 4");
       if (digits > 2) //length check
       {
         single_servo = false;
         
-        int s = servo_num*pow(10,digits-1);
-        servo_num2 = instring[1];
+        long s = servo_num*pow(10,digits-1);
+        char c = instring[1];
+        servo_num2 = (int)c -48;
         Serial.print("second servo: ");
         Serial.println(servo_num2);
         s += servo_num2*pow(10,digits-2);
@@ -121,13 +137,17 @@ void loop()
         Serial.print("desired postion: ");
         Serial.println(desire_pos);
         
-        cur_pos = GetPosition(servo_num);  
-        Serial.print("current position: ");
-        Serial.println(cur_pos);
+        Serial.print("current position for second servo: ");
+        Serial.println(GetPosition(servo_num2));
+        if (cur_pos != GetPosition(servo_num2))
+        {
+          SetPosition(servo_num2,1024-cur_pos);
+        }
+        Serial.println("-----------------------------------");
         
-        SetPosition(servo_num2,cur_pos);    
       }
     }
+    
     
     if (run)
     {
@@ -138,7 +158,10 @@ void loop()
          for(int i=cur_pos;i<desire_pos;i++)
          {
            SetPosition(servo_num,i);
-           if (!single_servo){SetPosition(servo_num2,i);}
+           if (!single_servo)
+           {
+             SetPosition(servo_num2,1024-i);
+           }
           }
           Serial.println("set");
           delay(1000);
@@ -148,16 +171,16 @@ void loop()
           for(int i= cur_pos; i > desire_pos; i--)
           {
             SetPosition(servo_num,i);
-            if (!single_servo){SetPosition(servo_num2,i);}
+            if (!single_servo){SetPosition(servo_num2,1024-i);}
           }
           Serial.println("set");
           delay(1000);
         }
         
         Serial.println("done");
-        Serial.println("--------------------------");
+        Serial.println("================================================");
     }
-    
+
   }
  
   
