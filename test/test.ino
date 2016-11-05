c45e0a6f387ccc581239a4ebc7cad3c64b64afa1
Yiyang Li <liyiyanglinch@gmail.com>
2016-11-05 18:55:16 -0400

    added test.no and Py file

---
+++ test/test.ino
@@ -0,0 +1,34 @@
+void setup(){
+  Serial.begin(38400); //start serial communications at 38400bps
+  Serial.setTimeout(2); 
+}
+
+
+void loop(){
+  delay(10);
+  if (Serial.available() > 0) {
+    if (Serial.available() == 4){
+      // A READ JOINTS COMMAND 
+      // READ ALL BYTES
+      //READ START BYTES (2 of them)
+      //READ END BYTES
+      // WRITE JOINTS OR SOMETHING FOR TESTING
+      Serial.println('THSFA');
+    }
+    
+    else if (Serial.available() == 14 ){
+      // A SET JOINTS COMMAND
+      // READ ALL BYTES
+      // CHECK START BYTES
+      // CHECK END BYTES
+      // DO THE BEHAVIOR
+    }
+    
+    else{ 
+    // FLUSH
+    }
+    
+    
+    
+  }
+}
